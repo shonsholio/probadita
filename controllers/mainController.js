fc0845d14f2f03fb66f59d6f261ec957db1ec28a
Luis Fernando Arevalo <LuisFerArevalo@Luiss-MacBook-Pro.local>
2024-03-20 14:05:54 -0500

    con la info

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,5 +1,5 @@
 const controller = {};
-const props = require('../src/props.json')
+const props = require('../src/props.json');
 
 controller.main = (req,res) => {
   res.render('main');
@@ -10,19 +10,5 @@ controller.luisfer = (req,res) => {
     data: props
   }); 
 }  
-
-  // const info = {
-  //                 propiedad: 409,
-  //                 ubicacion: 'Ciudad Jardin',
-  //                 habitacion: 1
-  // };
-
-  // const info = JSON.parse(props);
-
-  // const data = fs.readFileSync('./src/props.json', 'utf8');
-
-  // res.render('luisfer');
-  // info.forEach(element => {
-  // });
 
 module.exports = controller
